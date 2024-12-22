75aef1a47da338b60d344062ca689a420298759e
anurag kanwar <aigkaraig@gmail.com>
2024-12-22 11:57:13 +0530

    base

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import * as command from './command.js';
+
+const program = command.makeProgram();
+
+try {
+  program.parse(process.argv);
+} catch (err) {
+  if (err instanceof Error) {
+    if (program.opts().debug) {
+      console.log(`${err.stack}`);
+    }
+  } else {
+    throw err;
+  }
+  // Recommended practice for node is set exitcode not force exit
+  process.exitCode = 1;
+}
